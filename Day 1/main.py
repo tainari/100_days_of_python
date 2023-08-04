ac8133e1435b787a976c0c448a4a526517d9b630
tainari <alexandra.jacunski@gmail.com>
2023-08-04 12:50:03 -0400

    first commit

---
+++ Day 1/main.py
@@ -0,0 +1,11 @@
+
+
+print("Welcome to the Band Name Generator")
+
+city = input("What is the name of the city you grew up in?\n")
+
+pet = input("What is the name of your pet?\n")
+
+band_name = city + " " + pet
+
+print(f"Your band name could be {band_name}")
