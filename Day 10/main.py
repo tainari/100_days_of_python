c9ec2614bd56e1d6e75e6cfb98d7badcfbb73be8
tainari <alexandra.jacunski@gmail.com>
2023-08-12 10:37:31 -0400

    added multiple calculations in a row

--- Day 10/main.py
+++ Day 10/main.py
@@ -28,18 +28,25 @@ calc_dict = {
 print(logo)
 newstring  = "\n"
 num1 = int(input("What is the first number? "))
-operation = input(
-    f"Which operation do you want?\n {newstring.join(calc_dict.keys())}\n")
+operation = input(f"Which operation do you want? Options: {' '.join(calc_dict.keys())}\n")
 num2 = int(input("What is the second number? "))
 result = calc_dict[operation](num1,num2)
 print(f'{num1} {operation} {num2} = {result}')
-keep_going = False
-go = input("Keep going? Press any key + enter to keep going, or enter to quit")
-if go:
-    keep_going = True
-else:
-    
+keep_going = input(
+    "Keep going? Type yes to do another calculation. Type no to quit.\n").lower()
+keep_going = True if keep_going == 'yes' else False
 
-while keep_going = True:
+while keep_going == True:
+    num3 = int(input("What's the next number? \n"))
+    operation = input(
+        f"Which operation do you want? Options: {' '.join(calc_dict.keys())}\n")
+    #note to self: you can join with a newstring by using newstring.join(list)
+    # operation = input(f"Which operation do you want?\n {newstring.join(calc_dict.keys())}\n")
+    new_result = calc_dict[operation](result, num3)
+    print(f'{result} {operation} {num3} = {new_result}')
+    result = new_result
+    keep_going = input(
+        "Keep going? Type yes to do another calculation. Type no to quit.\n").lower()
+    keep_going = True if keep_going == 'yes' else False
 
-num3 = int(input("What's the next number? \n"))
+print("Goodbye.")
---
+++ Day 10/test_input_boolean.py
@@ -0,0 +1,5 @@
+x = input("Enter nothing and check truthiness.")
+if not x:
+    print("Huzzah.")
+else:
+    print("x exists.")
