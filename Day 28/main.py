65cf7d3b46329841ca3f1cd9dd1f08cd32eb6da2
tainari <alexandra.jacunski@gmail.com>
2023-09-05 12:56:57 -0400

    completed pomodoro timer

--- Day 28/main.py
+++ Day 28/main.py
@@ -12,38 +12,51 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 CHECK = "✅"
-
+running = True
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
-    pass
+    global running
+    running = False
+    pomodoros_complete_label["text"] = ""
+    canvas.itemconfig(time_display,text="00:00")
+    start.config(state="normal")
+    app_title["text"] = "Timer"
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
-num_pomodoros_complete = 0
-work_status = 1
-def start_timer(num_minutes=TEST_MIN):
-    countdown(int(num_minutes * 60))
+# num_pomodoros_complete = 0
+# work_status = 1
+def start_timer(num_minutes=WORK_MIN,num_pomodoros_complete=0,work_status=1):
+    global running
+    running = True
+    app_title["text"] = "Work" if work_status > 0 else "Rest"
+    start.config(state="disabled")
+    countdown(int(num_minutes * 60),num_pomodoros_complete,work_status)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
-def countdown(seconds_remaining):
-    if seconds_remaining > 0:
-        print(seconds_remaining)
-        minutes = seconds_remaining//60
-        minutes = minutes if minutes >= 10 else "0"+str(minutes)
-        seconds = seconds_remaining % 60
-        seconds = seconds if seconds >= 10 else "0"+str(seconds)
-        canvas.itemconfig(time_display,text=f"{minutes}:{seconds}")
-        window.after(1000, countdown, seconds_remaining-1)
-    else:
-        num_pomodoros_complete += 1
-        work_status *= -1
-        if work_status < 0:
-            if num_pomodoros_complete % 5 == 0:
-                start_timer(LONG_BREAK_MIN)
-            else:
-                start_timer(SHORT_BREAK_MIN)
+def countdown(seconds_remaining,num_pomodoros_complete = 0, work_status=1):
+    if running:
+        if seconds_remaining > 0:
+            minutes = seconds_remaining//60
+            minutes = minutes if minutes >= 10 else "0"+str(minutes)
+            seconds = seconds_remaining % 60
+            seconds = seconds if seconds >= 10 else "0"+str(seconds)
+            canvas.itemconfig(time_display,text=f"{minutes}:{seconds}")
+            window.after(1000, countdown, seconds_remaining-1,num_pomodoros_complete,work_status)
         else:
-            start_timer(WORK_MIN)
+            work_status *= -1
+            if work_status < 0:
+                window.tkraise()
+                num_pomodoros_complete += 1
+                pomodoros_complete_label["text"] = CHECK * num_pomodoros_complete
+                if num_pomodoros_complete % 5 == 0:
+                    start_timer(LONG_BREAK_MIN,0,-1)
+                else:
+                    start_timer(SHORT_BREAK_MIN,num_pomodoros_complete,-1)
+            else:
+                pomodoros_complete_label["text"] = CHECK * num_pomodoros_complete
+                start_timer(WORK_MIN,num_pomodoros_complete,work_status)
 
 
 
@@ -56,31 +69,21 @@ canvas = tkinter.Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
 bg_image = tkinter.PhotoImage(file="./tomato.png")
 canvas.create_image(100,112,image=bg_image)
 canvas.grid(row=1, column=1)
-# canvas.pack()
 
-# app_title = canvas.create_text(100,0,text="Timer",fill=GREEN,font=(FONT_NAME,40,"bold"))
 time_display = canvas.create_text(103,130,text="00:00",fill="white",font=(FONT_NAME,25,"bold"))
 
 app_title = tkinter.Label(text="Timer",foreground=GREEN,background=YELLOW,font=(FONT_NAME,40,"bold"),justify="center")
 app_title.grid(row=0, column=1)
-# app_title.place(x=50,y=-50)
 
 start = tkinter.Button(text="Start",command=start_timer,highlightbackground=YELLOW)#,highlightthickness=0,padx=0,pady=0,bd=0,borderwidth=0,background=YELLOW)
 start.grid(row=2,column=0)
-# start.place(x=-20,y=220)
 
 
 reset = tkinter.Button(text="Reset",command=reset_timer,highlightbackground=YELLOW)#,highlightthickness=0,padx=0,pady=0,bd=0,borderwidth=0,background=YELLOW)
 reset.grid(row=2,column=2)
-# reset.place(x=160,y=220)
 
-pomodoros_complete_label = tkinter.Label(text=CHECK, background=YELLOW, justify="center", font=(FONT_NAME,12),borderwidth=0)
+pomodoros_complete_label = tkinter.Label(text="", background=YELLOW, justify="center", font=(FONT_NAME,12),borderwidth=0)
 pomodoros_complete_label.grid(row=3,column=1)
-# pomodoros_complete_label.place(x=80,y=250)
-
-# window.minsize(width=500,height=500)
-# bg = tkinter.Label(image=bg_image)
-# bg.place(x=0,y=0)
 
 
 window.mainloop()
