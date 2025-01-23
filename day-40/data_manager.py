a738cd69f30351ce029937c7f43c65d8bebbbd6a
tainari <alexandra.jacunski@gmail.com>
2025-01-23 13:13:20 -0500

    Day 40 - jk using my base code not the starter code from the course

--- day-40/data_manager.py
+++ day-40/data_manager.py
@@ -1,42 +1,13 @@
-import os
-import requests
-from requests.auth import HTTPBasicAuth
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-SHEETY_PRICES_ENDPOINT = YOUR ENDPOINT HERE
-
-
+import json, requests
 class DataManager:
-
+    #This class is responsible for talking to the Google Sheet.
     def __init__(self):
-        self._user = os.environ["SHEETY_USRERNAME"]
-        self._password = os.environ["SHEETY_PASSWORD"]
-        self._authorization = HTTPBasicAuth(self._user, self._password)
-        self.destination_data = {}
-
-    def get_destination_data(self):
-        # Use the Sheety API to GET all the data in that sheet and print it out.
-        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
-        data = response.json()
-        self.destination_data = data["prices"]
-        # Try importing pretty print and printing the data out again using pprint() to see it formatted.
-        # pprint(data)
-        return self.destination_data
-
-    # In the DataManager Class make a PUT request and use the row id from sheet_data
-    # to update the Google Sheet with the IATA codes. (Do this using code).
-    def update_destination_codes(self):
-        for city in self.destination_data:
-            new_data = {
-                "price": {
-                    "iataCode": city["iataCode"]
-                }
-            }
-            response = requests.put(
-                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
-                json=new_data
-            )
-            print(response.text)
+        # Continuing to use JSON so that I don't have six million env variables
+        with open("../info.json") as f:
+            info = json.load(f)
+        sheety_workout_endpoint = info["sheety_flight_endpoint"]
+        sheety_headers = {
+            "Authorization": info["sheety_flight_token"],
+        }
+        response = requests.get(sheety_workout_endpoint, headers=sheety_headers)
+        self.data = response.json()['prices']
--- day-40/main.py
+++ day-40/main.py
@@ -1,62 +1,20 @@
-import time
-from datetime import datetime, timedelta
+#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
+from flight_data import FlightData
 from flight_search import FlightSearch
-from flight_data import find_cheapest_flight
 from notification_manager import NotificationManager
 
-# ==================== Set up the Flight Search ====================
+HOME_AIRPORT = "NYC"
 
-data_manager = DataManager()
-sheet_data = data_manager.get_destination_data()
-flight_search = FlightSearch()
+destination_data = DataManager()
 notification_manager = NotificationManager()
 
-# Set your origin airport
-ORIGIN_CITY_IATA = "LON"
-
-# ==================== Update the Airport Codes in Google Sheet ====================
-
-for row in sheet_data:
-    if row["iataCode"] == "":
-        row["iataCode"] = flight_search.get_destination_code(row["city"])
-        # slowing down requests to avoid rate limit
-        time.sleep(2)
-print(f"sheet_data:\n {sheet_data}")
-
-data_manager.destination_data = sheet_data
-data_manager.update_destination_codes()
-
-# ==================== Search for Flights and Send Notifications ====================
-
-tomorrow = datetime.now() + timedelta(days=1)
-six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
-
-for destination in sheet_data:
-    print(f"Getting flights for {destination['city']}...")
-    flights = flight_search.check_flights(
-        ORIGIN_CITY_IATA,
-        destination["iataCode"],
-        from_time=tomorrow,
-        to_time=six_month_from_today
-    )
-    cheapest_flight = find_cheapest_flight(flights)
-    print(f"{destination['city']}: £{cheapest_flight.price}")
-    # Slowing down requests to avoid rate limit
-    time.sleep(2)
-
-    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
-        print(f"Lower price flight found to {destination['city']}!")
-        # notification_manager.send_sms(
-        #     message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
-        #                  f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
-        #                  f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
-        # )
-        # SMS not working? Try whatsapp instead.
-        notification_manager.send_whatsapp(
-            message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
-                         f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
-                         f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
-        )
-
-
+flight_search = FlightSearch()
+for destination in destination_data.data:
+    destination_city = destination['city']
+    destination_airport = destination['iataCode']
+    max_price = destination['lowestPrice']
+    results = flight_search.check_route_price(destination_airport, max_price)['data']
+    if results:
+        flight_data = FlightData(results)
+        notification_manager.send_email(flight_data.format_data())
