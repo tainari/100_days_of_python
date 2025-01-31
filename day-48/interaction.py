c75c3d8bc7453321cffee4b1a61029729ab0a030
tainari <alexandra.jacunski@gmail.com>
2025-01-31 15:35:31 -0500

    day 48 intro to selenium - exploration

--- day-48/interaction.py
+++ day-48/interaction.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
-URL = "https://en.wikipedia.org/wiki/Main_Page"
+
+
+
+URL = "https://secure-retreat-92358.herokuapp.com"
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -11,8 +14,29 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
 
-# article_count_div = driver.find_element(By.ID, "articlecount")
-article_count = driver.find_element(By.XPATH, value='//*[@id="articlecount"]/ul/li[2]/a[1]')
-print(article_count.text)
+## Challenge: LAB Report signup
+first_name = driver.find_element(By.NAME, "fName")
+first_name.send_keys("John")
+last_name = driver.find_element(By.NAME, "lName")
+last_name.send_keys("Doe")
+email = driver.find_element(By.NAME, "email")
+email.send_keys("<EMAIL>")
+submit = driver.find_element(By.XPATH, "/html/body/form/button")
+submit.click()
 
-driver.quit()
+## EXPLORING WIKIPEDIA
+# URL = "https://en.wikipedia.org/wiki/Main_Page"
+# driver.get(URL)
+# article_count_div = driver.find_element(By.ID, "articlecount")
+# article_count = driver.find_elements(By.CSS_SELECTOR, "#articlecount a")[1]
+# # article_count = driver.find_element(By.XPATH, value='//*[@id="articlecount"]/ul/li[2]/a[1]')
+# # print(article_count.text)
+# article_count.click()
+
+# all_portals = driver.find_element(By.LINK_TEXT, "Forty-seven rōnin")
+# all_portals.click()
+
+# search_bar = driver.find_element(By.NAME, "search")
+# search_bar.send_keys("Python")
+# search_bar.send_keys(Keys.ENTER)
+# # driver.quit()
